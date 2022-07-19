4e81a0e6c502681a399eb0719fff592316dcf76f
George Tskitishvili <georgetskitishvili@outlook.com>
2022-07-19 17:15:52 +0400

    Added would you rather game

---
+++ commands/Fun/would-you-rather.js
@@ -0,0 +1,28 @@
+const SnakeGame = require('snakecord');
+module.exports = {
+    name: 'wouldyourather',
+    aliases: ['wya', 'rathergame', 'wouldrather', 'wyrather'],
+    description: 'Would you rather?',
+    category: 'Fun',
+    minArgs: 2,
+    expectedArgs: '<option1> <option2>',
+    guildOnly: true,
+    testOnly: true,
+    slash: false,
+    cooldown: '5s',
+    callback: async ({guild, member, user, interaction, message, channel, args, client, prefix, instance}) => {
+        const { WouldYouRather } = require('discord-gamecord')
+
+new WouldYouRather({
+  message: message,
+  slash_command: false,
+  embed: {
+    title: 'Would You Rather',
+    color: '#5865F2',
+  },
+  thinkMessage: '**Thinking...**',
+  buttons: { option1: args[0], option2: args[1]},
+  othersMessage: 'You are not allowed to use buttons for this message!',
+}).startGame();
+    }
+}
