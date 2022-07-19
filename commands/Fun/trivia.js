cf80c77bbd4b16d7cf7c82076a3c9ddb85f34b1f
George Tskitishvili <georgetskitishvili@outlook.com>
2022-07-19 18:01:09 +0400

    Added trivia command and added slash command support for would you rather game

---
+++ commands/Fun/trivia.js
@@ -0,0 +1,45 @@
+/* */
+const SnakeGame = require('snakecord');
+module.exports = {
+    name: 'wouldyourather',
+    aliases: ['wya', 'rathergame', 'wouldrather', 'wyrather'],
+    description: 'Would you rather?',
+    category: 'Fun',
+    guildOnly: true,
+    testOnly: true,
+    slash: 'both',
+    cooldown: '5s',
+    callback: async ({guild, member, user, interaction, message, channel, args, client, prefix, instance}) => {
+        const { Trivia } = require('discord-gamecord')
+
+      if(message) {
+         new Trivia({
+          message: message,
+          slash_command: false,
+          embed: {
+            title: 'Trivia',
+            description: 'You have {time} seconds to respond!',
+            color: '#5865F2',
+          },
+          difficulty: 'medium',
+          winMessage: 'GG, Your answer was correct! It was **{answer}**',
+          loseMessage: 'Your answer was Incorrect! The correct answer was **{answer}**',
+          othersMessage: 'You are not allowed to use buttons for this message!',
+        }).startGame(); 
+    } else if(interaction) {
+        new Trivia({
+            message: interaction,
+            slash_command: true,
+            embed: {
+              title: 'Trivia',
+              description: 'You have {time} seconds to respond!',
+              color: '#5865F2',
+            },
+            difficulty: 'medium',
+            winMessage: 'GG, Your answer was correct! It was **{answer}**',
+            loseMessage: 'Your answer was Incorrect! The correct answer was **{answer}**',
+            othersMessage: 'You are not allowed to use buttons for this message!',
+          }).startGame(); 
+    }
+    }
+}
--- commands/Fun/would-you-rather.js
+++ commands/Fun/would-you-rather.js
@@ -10,17 +10,32 @@ module.exports = {
     cooldown: '5s',
     callback: async ({guild, member, user, interaction, message, channel, args, client, prefix, instance}) => {
         const { WouldYouRather } = require('discord-gamecord')
-
+ if(message){
+    
 new WouldYouRather({
-  message: message,
-  slash_command: false,
-  embed: {
-    title: 'Would You Rather',
-    color: '#5865F2',
-  },
-  thinkMessage: '**Thinking...**',
-  buttons: { option1: 'Option 1', option2: 'Option 2'},
-  othersMessage: 'You are not allowed to use buttons for this message!',
-}).startGame();
+    message: message,
+    slash_command: false,
+    embed: {
+      title: 'Would You Rather',
+      color: '#5865F2',
+    },
+    thinkMessage: '**Thinking...**',
+    buttons: { option1: 'Option 1', option2: 'Option 2'},
+    othersMessage: 'You are not allowed to use buttons for this message!',
+  }).startGame();
+ } else if(interaction) {
+    
+new WouldYouRather({
+    message: interaction,
+    slash_command: true,
+    embed: {
+      title: 'Would You Rather',
+      color: '#5865F2',
+    },
+    thinkMessage: '**Thinking...**',
+    buttons: { option1: 'Option 1', option2: 'Option 2'},
+    othersMessage: 'You are not allowed to use buttons for this message!',
+  }).startGame();
+ }
     }
 }
