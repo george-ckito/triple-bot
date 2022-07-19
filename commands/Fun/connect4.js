f92db6e9c303c528bfcbf4b79232a282492ec648
George Tskitishvili <georgetskitishvili@outlook.com>
2022-07-19 17:25:43 +0400

    Added connect4 game

---
+++ commands/Fun/connect4.js
@@ -0,0 +1,68 @@
+const SnakeGame = require('snakecord');
+module.exports = {
+    name: 'connect4',
+    aliases: ['con4', 'conn4', 'connect', 'con'],
+    description: 'Connect 4 !!',
+    category: 'Fun',
+    guildOnly: true,
+    testOnly: true,
+    slash: 'both',
+    cooldown: '5s',
+    callback: async ({guild, member, user, interaction, message, channel, args, client, prefix, instance}) => {
+        const { Connect4 } = require('discord-gamecord')
+    if(message){
+        const { Connect4 } = require('discord-gamecord')
+
+new Connect4({
+  message: message,
+  slash_command: false,
+  opponent: message.mentions.users.first(),
+  embed: {
+    title: 'Connect 4',
+    color: '#5865F2',
+  },
+  emojis: {
+    player1: '🔵',
+    player2: '🟡'
+  },
+  waitMessage: 'Waiting for the opponent...',
+  turnMessage: '{emoji} | Its turn of player **{player}**.',
+  winMessage: '{emoji} | **{winner}** won the game!',
+  gameEndMessage: 'The game went unfinished :(',
+  drawMessage: 'It was a draw!',
+  othersMessage: 'You are not allowed to use buttons for this message!',
+  askMessage: 'Hey {opponent}, {challenger} challenged you for a game of Connect 4!',
+  cancelMessage: 'Looks like they refused to have a game of Connect4. \:(',
+  timeEndMessage: 'Since the opponent didnt answer, i dropped the game!',
+}).startGame()
+    } else if(interaction) {
+        const { Connect4 } = require('discord-gamecord')
+
+new Connect4({
+  message: message,
+  slash_command: false,
+  opponent: message.mentions.users.first(),
+  embed: {
+    title: 'Connect 4',
+    color: '#5865F2',
+  },
+  emojis: {
+    player1: '🔵',
+    player2: '🟡'
+  },
+  waitMessage: 'Waiting for the opponent...',
+  turnMessage: '{emoji} | Its turn of player **{player}**.',
+  winMessage: '{emoji} | **{winner}** won the game!',
+  gameEndMessage: 'The game went unfinished :(',
+  drawMessage: 'It was a draw!',
+  othersMessage: 'You are not allowed to use buttons for this message!',
+  askMessage: 'Hey {opponent}, {challenger} challenged you for a game of Connect 4!',
+  cancelMessage: 'Looks like they refused to have a game of Connect4. \:(',
+  timeEndMessage: 'Since the opponent didnt answer, i dropped the game!',
+  message: interaction,
+  slash_command: true,
+  opponent: interaction.options.getUser('user')
+}).startGame()
+    }
+    }
+}
