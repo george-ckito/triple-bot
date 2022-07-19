81ddec21d4096fccb6bc3b9839436cef8422d987
George Tskitishvili <georgetskitishvili@outlook.com>
2022-07-19 16:49:34 +0400

    Added snake command

---
+++ commands/Fun/snake.js
@@ -0,0 +1,20 @@
+const SnakeGame = require('snakecord');
+module.exports = {
+    name: 'snake',
+    aliases: ['snakegame'],
+    description: 'Play popular snake game',
+    category: 'Fun',
+    guildOnly: true,
+    testOnly: true,
+    slash: 'false',
+    cooldown: '5s',
+    callback: ({guild, member, user, interaction, message, channel, args, client, prefix, instance}) => {
+        const snakeGame = new SnakeGame({
+            title: 'Snake Game',
+            color: 'GREEN',
+            timestamp: false,
+            gameOverTitle: 'Game Over'
+        });
+        return await snakeGame.newGame(message);
+    }
+}
